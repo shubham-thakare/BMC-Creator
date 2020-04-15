2ed91722c680d867367d04ba2ff71b070ed81bfd
Shubham Thakare <shubham.thakare@successive.tech>
2020-04-15 20:07:41 +0530

    Added states in the context provider

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import { AppContext, contextWithoutState } from './utils/Context';
 const App = () => {
   const { Provider } = AppContext;
 
-  const [title, setTitle] = useState();
-  const [version, setVersion] = useState();
-  const [date, setDate] = useState();
+  const [title, setTitle] = useState('');
+  const [version, setVersion] = useState('');
+  const [date, setDate] = useState('');
+  const [keyPartners, setKeyPartners] = useState({});
+  const [keyActivities, setKeyActivities] = useState({});
+  const [keyResources, setKeyResources] = useState({});
+  const [valuePropositions, setValuePropositions] = useState({});
+  const [customerRelationships, setCustomerRelationships] = useState({});
+  const [channels, setChannels] = useState({});
+  const [customerSegments, setCustomerSegments] = useState({});
+  const [costStructure, setCostStructure] = useState({});
+  const [revenueStreams, setRevenueStreams] = useState({});
 
   const contextWithState = {
     ...contextWithoutState,
@@ -17,6 +26,24 @@ const App = () => {
     setVersion,
     date,
     setDate,
+    keyPartners,
+    setKeyPartners,
+    keyActivities,
+    setKeyActivities,
+    keyResources,
+    setKeyResources,
+    valuePropositions,
+    setValuePropositions,
+    customerRelationships,
+    setCustomerRelationships,
+    channels,
+    setChannels,
+    customerSegments,
+    setCustomerSegments,
+    costStructure,
+    setCostStructure,
+    revenueStreams,
+    setRevenueStreams,
   };
 
   return (
