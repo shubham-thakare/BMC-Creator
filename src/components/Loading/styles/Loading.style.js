1c1a941f855623d63f944e223cbddd2a593d220b
Shubham Thakare <shubhamthakare2429@gmail.com>
2020-04-16 13:57:33 +0530

    Code refactoring

--- src/components/Loading/styles/Loading.style.js
+++ src/components/Loading/styles/Loading.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Loader = styled.div`
+export const LoaderWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
