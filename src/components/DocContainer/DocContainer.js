cc2def6c4aa1a190da4c960f6f198b046e639a8f
Shubham Thakare <shubhamthakare2429@gmail.com>
2020-04-17 13:43:58 +0530

    Added NoteDialog component and refactored some code

--- src/components/DocContainer/DocContainer.js
+++ src/components/DocContainer/DocContainer.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { DocContainerWrapper } from './styles/DocContainer.style';
 import { Icon } from '@blueprintjs/core';
+import { AppContext } from '../../utils';
+import { OPEN_DIALOG } from '../../actions/actions';
 
 const DocContainer = ({
   children,
@@ -9,10 +12,12 @@ const DocContainer = ({
   hasEditOption = true,
   allowFullScreen = true,
 }) => {
+  const { dispatch } = useContext(AppContext);
+
   return (
     <DocContainerWrapper className={isEmpty && 'empty-state'}>
       {hasEditOption && (
-        <Icon icon="plus" iconSize={15} className="add-option" />
+        <Icon icon="plus" iconSize={15} className="add-option" onClick={() => dispatch({ action: OPEN_DIALOG })} />
       )}
       {allowFullScreen && (
         <Icon icon="fullscreen" iconSize={15} className="full-screen-option" />
@@ -22,5 +27,12 @@ const DocContainer = ({
     </DocContainerWrapper>
   );
 };
+
+DocContainer.propTypes = {
+  message: PropTypes.string,
+  isEmpty: PropTypes.bool,
+  hasEditOption: PropTypes.bool,
+  allowFullScreen: PropTypes.bool,
+}
 
 export default DocContainer;
