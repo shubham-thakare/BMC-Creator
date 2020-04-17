f690eb7513dd22147a3d7f32ef644bf2839fec61
Shubham Thakare <shubhamthakare2429@gmail.com>
2020-04-17 13:45:39 +0530

    Prettier formatting

--- src/components/NoteDialog/styles/NoteDialog.style.js
+++ src/components/NoteDialog/styles/NoteDialog.style.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-export const NoteDialogWrapper = styled.div`
-`;
+export const NoteDialogWrapper = styled.div``;
 
-export const Div = styled.div`
-`;
+export const Div = styled.div``;
