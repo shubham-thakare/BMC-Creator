9113dc46f252dae048deaf30d12191d91d8b56c5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-20 08:19:31 +0000

    Deploy shubham-thakare/BMC-Creator to github.com/shubham-thakare/BMC-Creator.git:gh-pages

---
+++ precache-manifest.476682caa6a3730f6e0dd56476988bb2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28daf14e16ffef961dc2244e41b227b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbd7eedd929b027b4c2a",
+    "url": "/static/css/2.499f928f.chunk.css"
+  },
+  {
+    "revision": "4a4d553633bf5eef6366",
+    "url": "/static/css/main.144c9cc8.chunk.css"
+  },
+  {
+    "revision": "bbd7eedd929b027b4c2a",
+    "url": "/static/js/2.d55efd23.chunk.js"
+  },
+  {
+    "revision": "b578178f33e8b4baed76c7b8f7ce2e07",
+    "url": "/static/js/2.d55efd23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7455b5c3124e043e2799",
+    "url": "/static/js/3.28639de1.chunk.js"
+  },
+  {
+    "revision": "4a4d553633bf5eef6366",
+    "url": "/static/js/main.b21bb878.chunk.js"
+  },
+  {
+    "revision": "b6e4f6ff56015f2ef861",
+    "url": "/static/js/runtime-main.04f8059f.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BMC-Creator/precache-manifest.89abe0d5c17e67d0fa46c5bb19e86121.js"
+  "/precache-manifest.476682caa6a3730f6e0dd56476988bb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BMC-Creator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
